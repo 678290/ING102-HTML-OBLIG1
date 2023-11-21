3d0cfaab0f6ae185965b8adf51e57e3b4323f640
Magnus <678288@stud.hvl.no>
2023-11-21 18:23:43 +0100

    endret header

--- Javascript/headfoot.js
+++ Javascript/headfoot.js
@@ -1,34 +1,40 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header>
-            <div class="topp">
-                <div class="logo">
-                    <h1>
-                        <a href="./KLEDELI.html">KLEDELI</a>
-                    </h1>
-                    <h2>For deling av klær</h2>
-                </div>
-                <div class="logginnknapp" onclick="document.getElementById('logginn').style.display='block'">Logg inn</div>
+        <header>
+        <div class="topp">
+            <div class="henthjelp">
+                <ul>
+                    <li><a href="./Hente.html">Utleveringssteder</a></li>
+                    <li><a href="./Kontaktoss.html">Kontakt oss</a></li>
+                </ul>
             </div>
-                <br>
-                <nav>
-                    <ul>
-                        <li><a href="./Barn.html">Se alt</a></li>
-                        <li class="dropdown">
-                            <a href="" id="kategorier">Kategorier</a>
-                            <ul class="dropdowncontent">
-                                <li><a href="./Yttertoy.html">Ytterklær</a></li>
-                                <li><a href="./Innetøy.html">Innerklær</a></li>
-                                <li><a href="./Undertøy.html">Undertøy</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./Baerekraft.html">Bærekraft</a></li>
-                        <li><a href="./Omoss.html">Om oss</a></li>
-                        <li><a href="./Medlemskap.html">Bli medlem</a></li>
-                    </ul>
-                </nav>
-            </header>
+            <div class="logo">
+                <h1>
+                    <a href="./KLEDELI.html">KLEDELI</a>
+                </h1>
+                <h2>For deling av klær</h2>
+            </div>
+            <div class="logginnknapp" onclick="document.getElementById('logginn').style.display='block'">Logg inn</div>
+        </div>
+            <br>
+            <nav>
+                <ul>
+                    <li><a href="./Barn.html">Se alt</a></li>
+                    <li class="dropdown">
+                        <a href="" id="kategorier">Kategorier</a>
+                        <ul class="dropdowncontent">
+                            <li><a href="./Yttertoy.html">Ytterklær</a></li>
+                            <li><a href="./Innetøy.html">Innerklær</a></li>
+                            <li><a href="./Undertøy.html">Undertøy</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./Baerekraft.html">Bærekraft</a></li>
+                    <li><a href="./Omoss.html">Om oss</a></li>
+                    <li><a href="./Medlemskap.html">Bli medlem</a></li>
+                </ul>
+            </nav>
+        </header>
         `
     }
 
